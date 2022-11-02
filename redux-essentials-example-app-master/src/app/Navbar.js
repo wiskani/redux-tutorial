dcc7362ecd54a1f33f4be484f6af983ff264bb10
wiskani <wiskani@icloud.com>
2022-11-02 19:00:32 -0400

    midlle part 4 using-data

--- redux-essentials-example-app-master/src/app/Navbar.js
+++ redux-essentials-example-app-master/src/app/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export const Navbar = () => {
   return (
     <nav>
@@ -7,6 +9,9 @@ export const Navbar = () => {
         <h1>Redux Essentials Example</h1>
 
         <div className="navContent">
+          <div className='navContent'>
+            <Link to="/">Posts</Link>
+          </div>
           <div className="navLinks"></div>
         </div>
       </section>
