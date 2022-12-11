c9dfdd4626451c045762cf3d26f828e186ae4d21
wiskani <wiskani@icloud.com>
2022-12-11 17:57:28 -0400

    mitad del 5

--- redux-essentials-example-app-master/src/app/Navbar.js
+++ redux-essentials-example-app-master/src/app/Navbar.js
@@ -1,19 +1,30 @@
 import React from 'react'
-
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { fetchNotifications } from '../features/notifications/notificationsSlice'
+
 export const Navbar = () => {
+  const dispatch = useDispatch()
+
+  const fetchNewNotifications = () => {
+    dispatch(fetchNotifications())
+
+  }
   return (
     <nav>
       <section>
         <h1>Redux Essentials Example</h1>
-
-        <div className="navContent">
-          <div className='navContent'>
+          <div className="navContent">
+           <div className="navLinks">
             <Link to="/">Posts</Link>
+            <Link to="/users">Users</Link>
+            <Link to="/notifications">Notifications</Link>
+            </div>
+            <button className="button" onClick={fetchNewNotifications}>
+              Refresh Notifications
+            </button>
           </div>
-          <div className="navLinks"></div>
-        </div>
       </section>
     </nav>
   )
